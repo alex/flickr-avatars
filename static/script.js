4d5eea78bbb070f1652fd92e219f9fcf27258c21
Armin Ronacher <armin.ronacher@active-4.com>
2010-02-15 02:53:22 +0100

    New images and improved design.

--- static/script.js
+++ static/script.js
@@ -1,20 +1,20 @@
 $(function() {
-    $('div.bigbox > div').each(function() {
-        $(this).hide();
-    });
+  $('div.bigbox > div').each(function() {
+    $(this).hide();
+  });
 
-    function showTab(name) {
-        var old = $('div.bigbox > div:visible');
-        function fadeIn() {
-            $('#' + name).fadeIn('fast');
-        }
-        old.length ? old.fadeOut('fast', fadeIn) : fadeIn();
-    }
+  function showTab(name) {
+    var old = $('div.bigbox > div:visible');
+    function fadeIn() { $('#' + name).fadeIn('fast'); }
+    old.length ? old.fadeOut('fast', fadeIn) : fadeIn();
+  }
 
-    showTab(document.location.hash.substring(1) || 'problem');
-    $('ul.navigation a').each(function() {
-        $(this).click(function() {
-            showTab(this.href.split('#', 2)[1]);
-        });
+  $('body').hide().fadeIn(1000);
+  showTab(document.location.hash.substring(1) || 'problem');
+
+  $('ul.navigation a').each(function() {
+    $(this).click(function() {
+      showTab(this.href.split('#', 2)[1]);
     });
+  });
 });
